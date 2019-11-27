fadc88fbce40b682084e3c4bcc7ac4b1e62f3b8d
Ryu <ryujt658@hanmail.net>
2019-11-27 16:02:14 +0900

    Update test_ffplayer.cpp

--- src/test_ffplayer/test_ffplayer/test_ffplayer.cpp
+++ src/test_ffplayer/test_ffplayer/test_ffplayer.cpp
@@ -5,13 +5,13 @@
 int main()
 {
     FFPlayer player;
-	player.open("D:/Work/test.mp4");
 
 	while (true) {
 		printf("TASK: ");
 		string line;
 		getline(cin, line);
 
+		if (line == "o") player.open("");
 		if (line == "c") player.close();
 		if (line == "p") player.play();
 		if (line == "a") player.pause();
